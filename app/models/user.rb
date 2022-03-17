5d56f800f8667c921e7f4e47b1d9e31981eae87f
theeugenechong <eugenechong20011@gmail.com>
2022-03-17 17:14:45 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
   
@@ -6,6 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
+                    
   #virtually creates password and password_confirmation attributes
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
@@ -15,5 +18,27 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end 
+  
+  # Returns a random token (22 char long).
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end 
+  
+  # Remembers a user in the database for persiston sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end 
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end 
 end
