191d4340b4bf4a004e54e0a77f3846ba22bac4dc
theeugenechong <eugenechong20011@gmail.com>
2022-03-19 16:59:51 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end 
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -11,11 +15,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     
+    get help_path
+    assert_select "title", full_title("Help")
     get contact_path
     assert_select "title", full_title("Contact")
-    
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "title", full_title("Sign Up")
+    
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit User")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+    get users_path
+    assert_select "title", full_title("All Users")
   end
 end
