89c6b21d123c85c0c6c601365c839e6b51dd0ea0
theeugenechong <eugenechong20011@gmail.com>
2022-03-26 00:26:48 +0000

    Add extra config vars for CarrierWave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
     config.fog_credentials = {
       #Config for Amazon S3
       :provider => 'AWS',
@@ -8,6 +10,8 @@ if Rails.env.production?
       :region => ENV['S3_REGION']
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.fog_public     = false
     config.fog_provider = 'fog/aws'
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
 end 
